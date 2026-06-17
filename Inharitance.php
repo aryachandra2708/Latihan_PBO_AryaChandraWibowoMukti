faaf524d320ab43b9f204a74b2552d5f7eb1a9c3
aryachandra2708 <aryag6920@gmail.com>
2026-06-17 05:56:39 +0000

    [Tahap 4] Mengimplementasikan inheritance untuk kelas anak

---
+++ Inharitance.php
@@ -0,0 +1,14 @@
+<?php
+
+class TiketRegular extends Tiket {
+    protected $tipeAudio;
+    protected $lokasiBaris;
+
+    public function hitungTotalHarga() {
+        // Logika hitung harga untuk regular
+    }
+    
+    public function tampilkanInfoFasilitas() {
+        // Logika tampilkan info
+    }
+}
